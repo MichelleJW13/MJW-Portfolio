47736dd7024c69ce6c793c8ea0b66d725da2054f
MichelleJW13 <133919234+MichelleJW13@users.noreply.github.com>
2023-09-22 20:07:14 -0500

    New page
    About page added

---
+++ About/script.js
@@ -0,0 +1,70 @@
+/*Header Animation*/
+const profileImage = document.getElementById('profile-image');
+
+function animateImage() {
+  profileImage.style.transform = 'translateY(-10px) scale(1.10)';
+  setTimeout(() => {
+    profileImage.style.transform = 'translateY(0) scale(1)';
+  }, 1000);
+}
+
+/* Dark Mode */
+
+  const darkModeToggle = document.getElementById('darkModeToggle');
+  const body = document.body;
+
+  darkModeToggle.addEventListener('click', toggleDarkMode);
+
+  function toggleDarkMode() {
+    body.classList.toggle('dark-mode');
+    darkModeToggle.textContent = body.classList.contains('dark-mode') ? 'Light Mode' : 'Dark Mode';
+  }
+
+/*Typing Container*/
+
+function typeText(element, text, speed) {
+  let charIndex = 0;
+  const typingInterval = setInterval(() => {
+    if (charIndex < text.length) {
+      element.textContent += text.charAt(charIndex);
+      charIndex++;
+    } else {
+      clearInterval(typingInterval);
+    }
+  }, speed);
+}
+
+const container = document.querySelector('.typing-container');
+const textToType = "\"The future belongs to those who believe in the beauty of their dreams.\" -  Eleanor Roosevelt";
+
+typeText(container, textToType, 100);
+
+/*Slide-In Animation*/
+
+const textElement = document.querySelector('.slide-in-text');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      textElement.classList.add('active');
+      observer.unobserve(entry.target);
+    }
+  });
+}, {
+  threshold: 0.5
+});
+
+observer.observe(textElement);
+
+/* Fetch Asynchronous Pup */
+
+const fetchButton = document.getElementById("fetch-button");
+const asyncImage = document.getElementById("async-image");
+
+fetchButton.addEventListener("click", () => {
+  asyncImage.style.opacity = 0;
+  setTimeout(() => {
+    asyncImage.src = "https://assets.codepen.io/10052609/shiloh-vector1-fetch.png";
+    asyncImage.style.opacity = 1;
+  }, 300); 
+});
